95b1530e0ae26fe1069a1723b48468744c42e1b2
sloser <bsloser@gmail.com>
2008-07-09 23:01:26 +0200

    tests for create_alias plugin

--- vendor/plugins/create_alias/lib/create_alias.rb
+++ vendor/plugins/create_alias/lib/create_alias.rb
@@ -12,15 +12,14 @@ module CreateAlias
 
     letters.each {|k,v| string = string.gsub(k,v)}
  
-    string = string.gsub(/[^\w\s\-\—]/,'')  # remove all non string except for whitespaces, dashes, and hyphens 
-    string = string.gsub(/(\s|_)+/, ' ')    # replace all consecutive whitespaces and underscores with a single whitespace 
-    string = string.strip                   # strip leading/trailing whitespace 
-    string = string.gsub('/\—/', '-')       # replace hyphens with dash 
-    string = string.gsub('/\s?-\s?/', '-')  # remove whitespace surrounding dashs 
-    string = string.gsub('/(-)+/', '-')     # replace all consecutive dashes with a single dash 
-    string = string.gsub(/\s/, '-')         # replace whitespace with hyphen 
-    string = string.downcase                # downcase
-    return string                           # return the modified string
+    string = string.gsub(/[^\w\s\-\—]/,'')    # remove all non string except for whitespaces, dashes, and hyphens 
+    string = string.gsub(/(\s|_|(-)+)+/, ' ') # replace all consecutive whitespaces and underscores and dashes with a single whitespace 
+    string = string.strip                     # strip leading/trailing whitespace 
+    string = string.gsub('/\—/', '-')         # replace hyphens with dash 
+    string = string.gsub('/\s?-\s?/', '-')    # remove whitespace surrounding dashs 
+    string = string.gsub(/\s/, '-')           # replace whitespace with hyphen 
+    string = string.downcase                  # downcase
+    return string                            # return the modified string
   end
   
 end
--- vendor/plugins/create_alias/test/create_alias_test.rb
+++ vendor/plugins/create_alias/test/create_alias_test.rb
@@ -8,13 +8,52 @@ class CreateAliasTest < Test::Unit::TestCase
   end
   
   def test_should_return_valid_alias_with_spaces
-    assert_equal "this-is-my-permalink", "this is my permalink".create_alias
-    assert_equal "another-space-test", "another   space  test".create_alias
-  end
-  
-  def test_should_return_valid_alias_without_spaces
-    assert_equal "today", "today".create_alias
-    assert_equal "bobs", "bob's".create_alias
-  end
+   assert_equal "this-is-my-permalink", "this is my permalink".create_alias
+   assert_equal "another-space-test", "another   space test".create_alias
+ end
+
+ def test_should_return_valid_alias_without_spaces
+   assert_equal "today", "today".create_alias
+   assert_equal "bobs", "bob's".create_alias
+ end
+
+ def test_should_return_valid_alias_without_dash
+   assert_equal "this-is-my-permalink", "this_is_my_permalink".create_alias
+   assert_equal "another-dash-test", "another_dash___test".create_alias
+ end
+
+ def test_should_return_valid_alias_in_downcase
+   assert_equal "this-is-my-permalink", "THIS is MY permalink".create_alias
+ end
+
+ def test_should_keep_dashes_and_remove_spaces
+   assert_equal "dont-forget-dashes-with-spaces", "Don't forget - dashes -- with  --  spaces".create_alias
+ end
+
+
+ def test_should_return_valid_alias_without_croatian_letters_all_in_downcases
+   assert_equal "cczsdj-cczsdj", "čćžšđ ČĆŽŠĐ".create_alias
+   assert_equal "xcxcxzxsxdjx-xcxcxzxsxdjx", "xčxćxžxšxđx-xČxĆxŽxŠxĐx".create_alias
+ end
+
+ def test_should_return_valid_alias_without_any_special_chars
+   assert_equal "test-test", "test'\"!?+*-test:;.&".create_alias
+ end
+
+ def test_should_return_valid_alias_with_numbers
+   assert_equal "test-1234", "test 1234".create_alias
+       assert_equal "1234", "1234".create_alias
+ end
+
+
+ ## we don't want the bang changes
+ ## !ATTENTION! test this test
+
+ def test_should_return_valid_permalink_without_bang
+   string1 = "this is my permalink"
+   string2 = "another   space  test"
+   #assert_not_equal  "this-is-my-permalink", string1.create_alias!
+   #assert_not_equal  "another-space-test", string2.create_alias!
+ end
   
 end
