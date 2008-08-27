0136bdde9a5b68a26c96537ef3a558108dae95ff
sloser <bsloser@gmail.com>
2008-08-27 12:13:40 +0200

    article_test

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,8 +1,26 @@
-require 'test_helper'
+require '../test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_presence_of_article_table
+    assert Article.all
   end
+  
+  def test_article_table_sturcture
+    
+    a = Article.first
+    assert a.title
+    assert a.article_code
+    assert a.show_on_homepage
+    assert a.show_forever
+
+  end
+
+  def test_article_belongs_to_categories
+    
+    a = Article.homepage_articles
+    assert a.category.title
+
+  end
+
 end
