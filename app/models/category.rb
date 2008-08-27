17486f056ad955f1175e76246c36ea38a56670c6
sloser <sloser@emusoft.org>
2008-08-27 11:56:53 +0200

    remove roots named_scope

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,7 @@
 class Category < ActiveRecord::Base
 	
 	has_many :articles
-	acts_as_tree :order=>"title", :foreign_key=>"parent_category_id"	
-
-  named_scope :roots, :conditions => "parent_category_id IS NULL"
+	acts_as_tree :order=>"title", :foreign_key=>"parent_category_id"	  
 
   validates_presence_of     :title  
   validates_uniqueness_of   :category_code	
